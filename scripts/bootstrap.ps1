0b4fc012bd72318e206af52f4223bf9c8d015297
Chris Engle <go2engle@gmail.com>
2023-03-02 04:25:00 -0800

    Update bootstrap.ps1

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,5 +1,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
 kubectl apply -f https://raw.githubusercontent.com/Go2Engle/gitops-demo/main/argocd/bootstrap/applications-sync.yaml
 kubectl apply -f https://raw.githubusercontent.com/Go2Engle/gitops-demo/main/argocd/bootstrap/namespaces-sync.yaml
 kubectl apply -f https://raw.githubusercontent.com/Go2Engle/gitops-demo/main/argocd/bootstrap/tools-sync.yaml
